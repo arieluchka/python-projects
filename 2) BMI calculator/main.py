a58294e3b87db76430e4976674d275a7037ae3cd
Ariel Agranovich <ariel.agra@gmail.com>
2023-05-11 16:03:16 +0300

    Fixes #2: Made the bmi a class and added a few improvments

--- 2) BMI calculator/main.py
+++ 2) BMI calculator/main.py
@@ -1,45 +1,57 @@
-lbs_to_kg_multiplication = 0.45359237
+class BmiCalculator:
 
+    def __init__(self):
+        self._height = None
+        self._weight = None
+        self._bmi_value = None
+        self._lbs_to_kg_multiplication = 0.45359237
 
-def choose_weight():
-    weight_type = input("what weight system do you use? (lbs/kgs) \n")
-    weight_value = float(input("what is your weight? \n"))
-    if weight_type == "lbs":
-        return weight_value * lbs_to_kg_multiplication
-    else:
-        return weight_value
+    def choose_weight(self):
+        weight_type = input("what weight system do you use? (lbs/kgs) \n")
+        weight_value = float(input("what is your weight? \n"))
+        if weight_type == "lbs":
+            self._weight = weight_value * self._lbs_to_kg_multiplication
+            return self._weight
+        else:
+            self._weight = weight_value
+            return self._weight
 
+    def choose_height(self):
+        height_type = input("what height system do you use? (feet/meters) \n")
+        if height_type == "feet":
+            height_value_feet = int(input("how many feet? \n"))
+            height_value_inches = int(input("inch? \n"))
+            self._height = ((height_value_inches + (height_value_feet * 12)) * 2.54) / 100
+            return self._height
+        else:
+            self._height = (float(input("whats you height? (in CM) \n"))) / 100
+            return self._height
 
-def choose_height():
-    height_type = input("what height system do you use? (feet/meters) \n")
-    if height_type == "feet":
-        height_value_feet = int(input("how many feet? \n"))
-        height_value_inches = int(input("inch? \n"))
-        return ((height_value_inches + (height_value_feet * 12)) * 2.54) / 100
-    else:
-        return (float(input("whats you height? (in CM) \n"))) / 100
+    def calculate_bmi(self):
+        self._bmi_value = round((self._weight / (self._height ** 2)), 2)
+        return self._bmi_value
 
+    # added the option to call the method while input a known bmi,
+    # to skip the whole calculating progress
 
-def calculate_bmi(weight, height):
-    return weight / (height ** 2)
-
-
-def bmi_interpretation(bmi_value):
-    if bmi_value < 18.5:
-        return "underweight"
-    elif bmi_value < 25:
-        return "normal weight"
-    elif bmi_value < 30:
-        return "overweight"
-    elif bmi_value < 35:
-        return "obese"
-    else:
-        return "clinically obese"
+    def bmi_interpretation(self, bmi=None):
+        if bmi is None:
+            bmi = self._bmi_value
+        if bmi < 18.5:
+            return "underweight"
+        elif bmi < 25:
+            return "normal weight"
+        elif bmi < 30:
+            return "overweight"
+        elif bmi < 35:
+            return "obese"
+        else:
+            return "clinically obese"
 
 
 if __name__ == '__main__':
     print("Welcome to the BMI calculator")
-    weight_in_kg = choose_weight()
-    height_in_m = choose_height()
-    bmi = round(calculate_bmi(weight_in_kg, height_in_m), 2)
-    print(f"Your bmi is {bmi}. It means hat you are {bmi_interpretation(bmi)}")
+    bmi_check = BmiCalculator()
+    bmi_check.choose_weight()
+    bmi_check.choose_height()
+    print(f"Your bmi is {bmi_check.calculate_bmi()}. It means hat you are {bmi_check.bmi_interpretation()}")
