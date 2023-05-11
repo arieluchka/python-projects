8bb16f89623227e0a8395d0ddb4aec60d9d725aa
Ariel Agranovich <ariel.agra@gmail.com>
2023-05-11 14:39:56 +0300

    splited the code into a function and main. added error handling

--- 1) bill calculator/main.py
+++ 1) bill calculator/main.py
@@ -1,8 +1,15 @@
-print ("Welcome to the tip calculator!")
-total_bill = float(input("What was the total bill? $"))
-no_of_people = int(input("How many people to split the bill? "))
-tip = int(input("Want to leave a tip? (choose a percentage, 0 for no tip) %" ))
+def tip_calculator():
+    while True:
+        try:
+            total_bill = float(input("What was the total bill? $"))
+            no_of_people = int(input("How many people to split the bill? "))
+            tip = int(input("Want to leave a tip? (choose a percentage, 0 for no tip) %" ))
+            ppp = ((total_bill + (total_bill * (tip / 100))) / no_of_people)
+            return ppp
+        except ValueError:
+            print("please make sure you insert numbers only!")
 
-# payment per person
-ppp = ((total_bill + (total_bill * (tip / 100))) / no_of_people)
-print(f'Each person needs to pay ${ppp}')
+
+if __name__ == '__main__':
+    print ("Welcome to the tip calculator!")
+    print(f'Each person needs to pay ${tip_calculator()}')
